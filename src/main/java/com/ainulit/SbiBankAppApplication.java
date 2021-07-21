d9fc7a368642595c04752bde78d66c5b809f6fe6
Ainul <aliainul9568@gmail.com>
2021-07-21 18:04:28 +0530

    addd map

--- src/main/java/com/ainulit/SbiBankAppApplication.java
+++ src/main/java/com/ainulit/SbiBankAppApplication.java
@@ -6,10 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SbiBankAppApplication {
-
+          Map<String,String> hm=new HashMap<>();
 	public static void main(String[] args) {
 		SpringApplication.run(SbiBankAppApplication.class, args);
 		System.out.println("Welcome App..........");
 	}
 
+
+       public void loadDataToCache()
+{
+}
 }
