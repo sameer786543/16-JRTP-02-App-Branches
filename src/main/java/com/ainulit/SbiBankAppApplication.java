8a0c73eadf0eee80bbb9ee728be8f3be3f36e0e7
Ainul <aliainul9568@gmail.com>
2021-07-21 17:36:28 +0530

    first commit

---
+++ src/main/java/com/ainulit/SbiBankAppApplication.java
@@ -0,0 +1,15 @@
+
+package com.ainulit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbiBankAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbiBankAppApplication.class, args);
+		System.out.println("Welcome App..........");
+	}
+
+}
